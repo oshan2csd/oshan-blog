880c519a7269fde53206e203d012235cf72309a5
oshan2csd <oshan2csd@gmail.com>
2021-07-24 12:42:43 +1000

    adding styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,13 @@ export default function Navbar(){
             <h1>Oshan's Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">Create New</a>
                 <a href="/contact">Contact</a>
                 <a href="/about">About</a>
+                <a href="/create" style = {{
+                    color:'white',
+                    borderRadius:'8px',
+                    backgroundColor:'#6735f1'
+                }}>Create New</a>
             </div>
         </nav>
     );
