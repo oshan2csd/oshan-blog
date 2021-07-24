125f05f6bcd98b46731deee6cf357afe10e49a6c
oshan2csd <oshan2csd@gmail.com>
2021-07-24 12:21:12 +1000

    set up react project and basic component for the blog

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <h1>Oshan's Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create New</a>
+                <a href="/contact">Contact</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+} 
